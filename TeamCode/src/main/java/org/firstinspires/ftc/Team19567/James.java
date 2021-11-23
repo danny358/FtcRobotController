6ea9e3dc6075c45bcb0ac6c2247b853028785645
jameshu <hurex246@gmail.com>
2021-11-22 19:19:45 -0600

    James File created (by Ryan)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/Team19567/James.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.Team19567;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="JamesOP", group="Iterative Opmode")
+public class James extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
