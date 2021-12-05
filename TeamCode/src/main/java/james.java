e852f7d84c0cd3ad3c3be69a155c33f96db6f084
jameshu <hurex246@gmail.com>
2021-12-05 14:50:51 -0600

    changes to james

--- TeamCode/src/main/java/james.java
+++ TeamCode/src/main/java/james.java
@@ -7,40 +7,33 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name="JAMES", group="Iterative Opmode")
 public class james extends OpMode {
-    DcMotor LB = null;
-    DcMotor LF = null;
-    DcMotor RB = null;
-    DcMotor RF = null;
-
-//note: telemetry will be implemented asap
-//Mecanum Drive...
-
-
+    private DcMotor leftBack = null;
+    private DcMotor leftFront = null;
+    private DcMotor rightBack = null;
+    private DcMotor rightFront = null;
 
     @Override
-
     public void init() {
-        LB = hardwareMap.get(DcMotor.class, "LB");
-        LF = hardwareMap.get(DcMotor.class, "LF");
-        RB = hardwareMap.get(DcMotor.class, "RB");
-        RF = hardwareMap.get(DcMotor.class, "RF");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
     }
-    @Override
 
-    public void start () {
+    @Override
+    public void start() {
         telemetry.addData("status","Started" );
         telemetry.update();
-        RB.setDirection(DcMotorSimple.Direction.REVERSE); //sets R motors to reverse IF we need... and I dont know about the L motors
-        RF.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE); //sets R motors to reverse IF we need... and I dont know about the L motors
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
     }
     public void init_loop() {
         telemetry.addData("status","init_loop" );
         telemetry.update();
     }
+
     @Override
-
-    public void  loop() {
-
+    public void loop() {
         double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
         double RA = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
 
@@ -51,20 +44,15 @@ public class james extends OpMode {
         final double L2 = (r * Math.cos(RA) - gamepad1.right_stick_x);
         final double R2 = (r * Math.sin(RA) + gamepad1.right_stick_x);
 
-        LF.setPower(L1);
-        RF.setPower(R1);
-        LB.setPower(L2);
-        RB.setPower(R2);
-//commented out tank drivetrain mode ensenstiallyyyyE (no strafing just turning)
-        //double Up = -gamepad1.left_stick_y;
-//double Side = gamepad1.left_stick_x;
-//double pivot = gamepad1.right_stick_x;
-//RF.setPower(Skyblock + (-Up + Side));
-//RB.setPower(Skyblock + (-Up - Side));
-//LF.setPower(Skyblock + (-Up - Side));
-//LB.setPower(Skyblock + (-Up + Side));
+        leftFront.setPower(L1);
+        rightFront.setPower(R1);
+        leftBack.setPower(L2);
+        rightBack.setPower(R2);
+    }
+
+    @Override
+    public void stop() {
+        telemetry.addData("status", "stopped");
+        telemetry.update();
     }
 }
-
-
-
