5f3f36e88ae63b5a74ba79537fa82d951ec9b5a6
Isaacm05 <thebosssquidmei@gmail.com>
2022-02-01 20:46:20 -0600

    Carousel Test added exp test

--- TeamCode/src/main/java/org/firstinspires/ftc/Team19567/CarouselTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/Team19567/CarouselTest.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
 
 
 @TeleOp(name="Carousel Test", group="Iterative Opmode")
@@ -13,9 +14,11 @@ public class CarouselTest extends OpMode{
     private DcMotor carouselRight = null;
     private Mechanisms mechanisms = null;
     private final ElapsedTime carouselTime = new ElapsedTime();
-    private int milliCarousel = 2000;
-    private double initPower = 0.7;
-    private double finalPower = 1;
+    public int milliCarousel = 2000;
+    public double initPower = 0.6;
+    public double finalPower = 6;
+    public double expStartPoint = 1000;
+    public double expCoefficient = 50;
 
 
     @Override
@@ -41,35 +44,30 @@ public class CarouselTest extends OpMode{
     @Override
     public void loop() {
 
-        if(gamepad2.right_trigger > 0 || gamepad1.dpad_right || gamepad1.dpad_left){
+        if(gamepad2.right_trigger > 0 || gamepad1.dpad_right || gamepad1.dpad_left)
+        {
             carouselTime.reset();
             if(carouselTime.milliseconds() <= milliCarousel) mechanisms.rotateCarousel(initPower);
             else mechanisms.rotateCarousel(finalPower);
         }
 
-        if(gamepad1.right_trigger > 0){
-            milliCarousel += 200;
-        }
-        else if(gamepad1.right_bumper){
-            milliCarousel -= 200;
-        }
+        /*
+        if(gamepad2.right_trigger > 0 || gamepad1.dpad_right || gamepad1.dpad_left)
+        {
+            carouselTime.reset();
+            if(carouselTime.milliseconds() <= milliCarousel) mechanisms.rotateCarousel(initPower);
+            else mechanisms.rotateCarousel(Range.clip(((carouselTime.milliseconds() - expStartPoint/expCoefficient) + initPower)**2,initPower, finalPower);
 
-        else if(gamepad1.left_trigger > 0){
-            initPower += 0.05;
         }
+         */
 
-        else if(gamepad1.left_bumper){
-            initPower -= 0.05;
-        }
-
-        else if(gamepad2.dpad_down){
-            finalPower -= 0.05;
-        }
 
         telemetry.addData("Status", "Looping");
         telemetry.addData("Carousel Runtime ", milliCarousel + " Milliseconds");
         telemetry.addData("Initial Carousel Speed ", initPower);
         telemetry.addData("Final Carousel Speed ", finalPower);
+        telemetry.addData("Exponential Delay ", expStartPoint);
+        telemetry.addData("Exponential Coefficient" , expCoefficient);
         telemetry.update();
 
     }
