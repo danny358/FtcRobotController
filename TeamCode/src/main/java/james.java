2a3502da32ef89ebe43fde95134300e0e61ac917
jameshu <hurex246@gmail.com>
2022-02-08 21:30:48 -0600

    Added rumble effects v1

--- TeamCode/src/main/java/james.java
+++ TeamCode/src/main/java/james.java
@@ -1,58 +1,56 @@
-package org.firstinspires.ftc.Team19567;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+public class james extends LinearOpMode
+{
 
+    boolean highLevel = false;                  // used to prevent multiple level-based rumbles.
 
-@TeleOp(name="JAMES", group="Iterative Opmode")
-public class james extends OpMode {
-    private DcMotor leftBack = null;
-    private DcMotor leftFront = null;
-    private DcMotor rightBack = null;
-    private DcMotor rightFront = null;
+    Gamepad.RumbleEffect BoxSecured;
+    Gamepad.RumbleEffect endgamerumble;
+    ElapsedTime runtime = new ElapsedTime();    // How endgame time is determined.
+
+    final double END_GAME = 90;
+    final double HALF_TIME = 60.0;
+
 
     @Override
-    public void init() {
-        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
-        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
-        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
-        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
-    }
+    public void runOpMode()
+    {
+        // Example 1. a)   start by creating a three-pulse rumble sequence: right, LEFT, LEFT
+        endgamerumble = new Gamepad.RumbleEffect.Builder()
+                .addStep(0.0, 1.0, 500)  //  Rumble right motor 100% for 500 mSec
+                .addStep(0.0, 0.0, 300)  //  Pause for 300 mSec
+                .addStep(1.0, 0.0, 250)  //  Rumble left motor 100% for 250 mSec
+                .addStep(0.0, 0.0, 250)  //  Pause for 250 mSec
+                .addStep(1.0, 0.0, 250)  //  Rumble left motor 100% for 250 mSec
+                .build();
 
-    @Override
-    public void start() {
-        telemetry.addData("status","Started" );
-        telemetry.update();
-        rightBack.setDirection(DcMotorSimple.Direction.REVERSE); //sets R motors to reverse IF we need... and I dont know about the L motors
-        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
-    }
-    public void init_loop() {
-        telemetry.addData("status","init_loop" );
-        telemetry.update();
-    }
+        BoxSecured = new Gamepad.RumbleEffect.Builder()
+                .addStep(0.0, 1.0, 1000)  //  Rumble right motor 100% for one whole seconds
 
-    @Override
-    public void loop() {
-        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
-        double RA = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+                .build();
 
-        // caculates the amount of power we need in motors...
 
-        final double L1 = (r * Math.sin(RA) - gamepad1.right_stick_x);
-        final double R1 = (r * Math.cos(RA) + gamepad1.right_stick_x);
-        final double L2 = (r * Math.cos(RA) - gamepad1.right_stick_x);
-        final double R2 = (r * Math.sin(RA) + gamepad1.right_stick_x);
 
-        leftFront.setPower(L1);
-        rightFront.setPower(R1);
-        leftBack.setPower(L2);
-        rightBack.setPower(R2);
-    }
+        waitForStart();
+        runtime.reset();    // Start game timer.
 
-    @Override
-    public void stop() {
-        telemetry.addData("status", "stopped");
-        telemetry.update();
+
+            if ((runtime.seconds() > END_GAME))   {
+            gamepad1.runRumbleEffect(endgamerumble);
+
+        }
+
+        if((runtime.milliseconds() >= 85000 && runtime.milliseconds() <= 90000) || (runtime.milliseconds() >= 115000))
+        {
+            gamepad1.runRumbleEffect(BoxSecured);
+
+        }
+        else {
+            gamepad1.stopRumble();
+        }
     }
 }
