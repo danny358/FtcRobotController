2267379219aeef8511d35a127a6fa6d14f44b44a
jameshu <hurex246@gmail.com>
2022-02-08 21:48:16 -0600

    Added rumble effects v1

--- TeamCode/src/main/java/james.java
+++ TeamCode/src/main/java/james.java
@@ -3,16 +3,19 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 public class james extends LinearOpMode
 {
-
+    private DistanceSensor distanceSensor = null;
     boolean highLevel = false;                  // used to prevent multiple level-based rumbles.
 
-    Gamepad.RumbleEffect BoxSecured;
-    Gamepad.RumbleEffect endgamerumble;
+    Gamepad.RumbleEffect boxSecured;
+    Gamepad.RumbleEffect endGameRumble;
     ElapsedTime runtime = new ElapsedTime();    // How endgame time is determined.
 
-    final double END_GAME = 90;
+    final double END_GAME = 90; //useless for nwo
     final double HALF_TIME = 60.0;
 
 
@@ -20,7 +23,7 @@ public class james extends LinearOpMode
     public void runOpMode()
     {
         // Example 1. a)   start by creating a three-pulse rumble sequence: right, LEFT, LEFT
-        endgamerumble = new Gamepad.RumbleEffect.Builder()
+        endGameRumble = new Gamepad.RumbleEffect.Builder()
                 .addStep(0.0, 1.0, 500)  //  Rumble right motor 100% for 500 mSec
                 .addStep(0.0, 0.0, 300)  //  Pause for 300 mSec
                 .addStep(1.0, 0.0, 250)  //  Rumble left motor 100% for 250 mSec
@@ -28,7 +31,7 @@ public class james extends LinearOpMode
                 .addStep(1.0, 0.0, 250)  //  Rumble left motor 100% for 250 mSec
                 .build();
 
-        BoxSecured = new Gamepad.RumbleEffect.Builder()
+        boxSecured = new Gamepad.RumbleEffect.Builder()
                 .addStep(0.0, 1.0, 1000)  //  Rumble right motor 100% for one whole seconds
 
                 .build();
@@ -38,15 +41,17 @@ public class james extends LinearOpMode
         waitForStart();
         runtime.reset();    // Start game timer.
 
+        if(distanceSensor.getDistance(DistanceUnit.MM) <= 80) {
 
-            if ((runtime.seconds() > END_GAME))   {
-            gamepad1.runRumbleEffect(endgamerumble);
-
+            gamepad1.runRumbleEffect(boxSecured);
         }
+        /* else if(potentiometer.getVoltage() >=  SOME CONSTANT ) {
+            gamepad1.runRumbleEffect(boxSecured);
+        } */
 
-        if((runtime.milliseconds() >= 85000 && runtime.milliseconds() <= 90000) || (runtime.milliseconds() >= 115000))
+        else if((runtime.milliseconds() >= 85000 && runtime.milliseconds() <= 90000) || (runtime.milliseconds() >= 115000))
         {
-            gamepad1.runRumbleEffect(BoxSecured);
+            gamepad1.runRumbleEffect(endGameRumble);
 
         }
         else {
